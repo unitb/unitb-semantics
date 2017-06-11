b98f3bc39ea0afb545ce3c2e7ce52252f1314872
Simon Hudon <simon.hudon@gmail.com>
2017-06-11 17:44:06 -0400

    strengthened the rescheduling refinement rule by replacing leads-to with “often implies often”

--- unity/models/refinement/reschedule.lean
+++ unity/models/refinement/reschedule.lean
@@ -14,9 +14,18 @@ variables {α : Type}
 
 structure evt_ref (mc : program α) (ea ec : event α) : Prop :=
   (sim : ⟦ ec.step_of ⟧ ⟹ ⟦ ea.step_of ⟧)
-  (delay : ea.coarse_sch && ea.fine_sch ↦ ec.coarse_sch in mc)
+  (delay : ea.coarse_sch && ea.fine_sch  >~>  ec.coarse_sch || - ea.coarse_sch in mc)
   (stable : unless mc ec.coarse_sch (-ea.coarse_sch))
-  (resched : ea.coarse_sch && ea.fine_sch ↦ ec.fine_sch in mc)
+  (resched : ea.coarse_sch && ea.fine_sch  >~>  ec.fine_sch || - ea.coarse_sch in mc)
+
+structure evt_ref_piecewise (mc : program α) (ea ec : event α)
+    {n : ℕ} (ccsch : fin n → pred α) : Prop :=
+  (ccsch_def : ec.coarse_sch = (∀∀ i, ccsch i))
+  (sim : ⟦ ec.step_of ⟧ ⟹ ⟦ ea.step_of ⟧)
+  (delay : ∀ i, ea.coarse_sch && ea.fine_sch  >~>  ccsch i || - ea.coarse_sch in mc)
+  (stable : ∀ i, unless mc (ccsch i) (-ea.coarse_sch))
+  (resched : ea.coarse_sch && ea.fine_sch  >~>  ec.fine_sch || - ea.coarse_sch in mc)
+
 
 open temporal
 
@@ -24,9 +33,11 @@ structure refined (ma mc : program α) : Prop :=
   (bij : mc.lbl = ma.lbl)
   (sim_init : mc^.first ⟹ ma^.first)
   (sim' : ∀ e, ⟦ mc.step_of e ⟧ ⟹ action (ma.step_of (e.cast bij) ))
-  (delay : ∀ e, ma^.coarse_sch_of e && ma^.fine_sch_of e ↦ mc^.coarse_sch_of (e.cast' bij) in mc)
+  (delay : ∀ e, ma.coarse_sch_of e && ma.fine_sch_of e
+            >~> mc.coarse_sch_of (e.cast' bij) || - ma.coarse_sch_of e in mc)
   (stable : ∀ e, unless mc (mc^.coarse_sch_of e) (-ma^.coarse_sch_of (e.cast bij)))
-  (resched : ∀ e, ma^.coarse_sch_of e && ma^.fine_sch_of e ↦ mc^.fine_sch_of (e.cast' bij) in mc)
+  (resched : ∀ e, ma^.coarse_sch_of e && ma^.fine_sch_of e
+              >~> mc^.fine_sch_of (e.cast' bij) || - ma.coarse_sch_of e in mc)
 
 lemma refined.sim {m₀ m₁ : program α} (R : refined m₀ m₁)
 : ⟦ is_step m₁ ⟧ ⟹ ⟦ is_step m₀ ⟧ :=
@@ -42,6 +53,13 @@ begin
   rw H'' at H',
   apply H',
 end
+
+lemma piecewise_event_refinement {mc : program α}
+  {ea ec : event α}
+  {n : ℕ} {ccsch : fin n → α → Prop}
+  (H : evt_ref_piecewise mc ea ec ccsch)
+: evt_ref mc ea ec :=
+sorry
 
 lemma event_refinement {ma mc : program α}
    (BIJ : mc.lbl = ma.lbl)
@@ -63,7 +81,7 @@ begin
     { intro e,
       cases e with e,
       simp [ program.coarse_sch_of_none,program.fine_sch_of_none,cast_none'],
-      try { apply True_leads_to_True },
+      try { apply True_often_imp_often_True },
       try { apply True_unless } },
   { simp [cast_some'],
     apply (EVT e).delay },
@@ -96,7 +114,10 @@ begin
       apply FINE₀, },
     assert COARSE₁ : (<>[]•program.coarse_sch_of mc e') τ,
     { assert COARSE₂ : ([]<>•program.coarse_sch_of mc e') τ,
-      { apply inf_often_of_leads_to (system_sem.leads_to_sem (R.delay e) _ M₁),
+      { revert COARSE₀,
+        rw [imp_iff_not_or,p_not_eq_not,not_eventually,not_henceforth,not_init],
+        rw [-p_or_to_fun,p_or_comm,-inf_often_p_or],
+        apply (system_sem.often_imp_often_sem' _ M₁ (R.delay e)),
         apply CF_SCH },
       note UNLESS := unless_sem_str _ M₁.safety (R.stable e') COARSE₂,
       cases UNLESS with UNLESS H,
@@ -107,7 +128,10 @@ begin
           apply H },
         cases H' COARSE₀, } },
     assert FINE₁ : ([]<>•program.fine_sch_of mc e') τ,
-    { apply inf_often_of_leads_to (system_sem.leads_to_sem (R.resched _) _ M₁),
+    { revert COARSE₀,
+      rw [imp_iff_not_or,p_not_eq_not,not_eventually,not_henceforth,not_init],
+      rw [-p_or_to_fun,p_or_comm,-inf_often_p_or],
+      apply system_sem.often_imp_often_sem' _ M₁ (R.resched _),
       apply CF_SCH, },
     apply inf_often_entails_inf_often _ _ (M₁.liveness _ COARSE₁ FINE₁),
     note H := R.sim' e',
