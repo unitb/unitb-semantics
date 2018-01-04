9c848a2ae26efe01aa76751f34f79913acbe45e7
Simon Hudon <simon.hudon@gmail.com>
2018-01-04 14:36:10 -0500

    Ported the multi-event deterministic model to the new temporal logic

--- src/unitb/models/det.lean
+++ src/unitb/models/det.lean
@@ -17,12 +17,12 @@ structure program (lbl : Type) (α : Type) : Type :=
   (step : lbl → α → α)
 
 @[reducible]
-def pred (α : Sort*) := α → Prop
+def pred (α : Sort*) := pred' α
 
 variables {lbl α : Type}
 
 def program.init (s : program lbl α) (p : pred α) : Prop :=
-p (s^.first)
+(s^.first) ⊨ p
 
 def program.take_step (s : program lbl α) : option lbl → α → α
   | none := id
@@ -35,34 +35,30 @@ def is_step (s : program lbl α) (σ σ' : α) : Prop :=
 ∃ ev, s.action_of ev σ σ'
 
 def program.falsify (s : program lbl α) (ev : option lbl) (p q : pred α) : Prop :=
-∀ σ, q σ → ¬ (q (s^.take_step ev σ))
+∀ σ, σ ⊨ q → (s^.take_step ev σ) ⊨ - q
 
 def program.transient (s : program lbl α) (p q : pred α) : Prop :=
 ∃ ev, s.falsify ev p q
 
 def program.falsify_action (s : program lbl α) (p q : pred α) (ev : option lbl)
   (h : s.falsify ev p q)
-: •q ⟹ ( ⟦ s.action_of ev ⟧ ⟶ ⟦ λ _, - q ⟧ ) :=
-begin
-  intros τ Hq H₁,
-  unfold temporal.action program.action_of at H₁,
-  unfold temporal.action,
-  rw H₁,
-  apply (h _ Hq),
+: •q ⟹ ( ⟦ s.action_of ev ⟧ ⟶ ⊙ - •q ) :=
+begin [temporal]
+  action {
+    intros Hq H₁,
+    unfold program.action_of at H₁,
+    rw H₁,
+    apply (h _ Hq) },
 end
 
 lemma program.falsify.negate
    {s : program lbl α} {act : option lbl} {p q : pred' α}
-:  s.falsify act p q
-→  •q ⋀ ⟦ s^.action_of act ⟧ ⟹ ◇-•q :=
-begin
-  intros FALSE τ H,
-  have GOAL : ⟦ λ _, - q ⟧ τ,
-  { cases H with H₀ H₁,
-    apply s.falsify_action p q act FALSE _ H₀ H₁, },
-  unfold temporal.eventually,
-  existsi 1,
-  apply GOAL
+   (H : s.falsify act p q)
+:  •q ⋀ ⟦ s^.action_of act ⟧ ⟹ ◇-•q :=
+begin [temporal]
+  intros H',
+  strengthen_to ⊙-• _ ,
+  exact program.falsify_action s p q act H Γ H'.left H'.right,
 end
 
 lemma program.transient_impl (s : program lbl α) (p : pred' α)
@@ -74,7 +70,7 @@ begin
   cases h
 end
 
-lemma program.transient_antimono (s : program lbl α) {p q p' q' : α → Prop}
+lemma program.transient_antimono (s : program lbl α) {p q p' q' : pred α}
   (hp : p' ⟹ p)
   (hq : q' ⟹ q)
   (T₀ : s.transient p q)
@@ -84,9 +80,9 @@ begin
   cases T₀ with ev T₁,
   existsi ev,
   intros σ,
-  imp_transitivity [q σ,¬q (program.take_step s ev σ)],
-  intros_mono, apply hq,
-  apply T₁, apply hq,
+  imp_transitivity [σ ⊨ q,program.take_step s ev σ ⊨ -q], simp,
+  intros_mono, apply ew_str hq,
+  apply T₁, apply ew_str hq,
 end
 
 instance prog_is_system : unitb.system (program lbl α) :=
@@ -102,41 +98,52 @@ open temporal
 
 structure ex (p : program lbl α) (τ : stream α) : Prop :=
     (init : τ 0 = p^.first)
-    (safety : unitb.saf_ex p τ)
-    (liveness : ∀ e, (◻ ◇ ⟦ p.action_of e ⟧) τ)
+    (safety : τ ⊨ unitb.saf_ex p)
+    (liveness : ∀ e, τ ⊨ ◻ ◇ ⟦ p.action_of e ⟧)
 
 open unitb
 open nat
 
 section soundness
 
-variables {s : program lbl α} {p q : pred' α}
-variables (τ : stream α)
-variables h : ex s τ
+variables (s : program lbl α)
+variables (Γ : cpred α)
+variables {p q : pred' α}
+variables h : Γ ⊢ ex s
+
+lemma safety : Γ ⊢ ex s ⟶ unitb.saf_ex s :=
+by { apply p_imp_ext,
+     introv _, apply ex.safety, }
+lemma liveness : Γ ⊢ ex s ⟶ ∀∀ e, ◻ ◇ ⟦ s.action_of e ⟧ :=
+by { apply p_imp_ext,
+     introv _, apply ex.liveness, }
+
+variables {s}
 include h
 
 lemma init_sem
   (I₀ : init s p)
-: (•p) τ :=
-begin
-  unfold temporal.init,
-  rw h.init,
-  apply I₀
+: Γ ⊢ •p :=
+begin [temporal]
+  explicit τ {
+    rw h.init,
+    apply I₀ }
 end
 
 lemma transient.semantics
   (T₀ : s.transient p q)
-: (◻◇•p) τ → (◻◇-•q) τ :=
-begin
+: Γ ⊢ ◻◇•p ⟶ ◻◇-•q :=
+begin [temporal]
   intro,
-  unfold program.transient at T₀,
+  simp [program.transient] at T₀,
   cases T₀ with ev T₀,
-  cases temporal.em' (• q) τ with hq hnq,
-  { have occ := coincidence hq (h.liveness ev),
-    rw ← eventually_eventually,
-    revert_p occ, monotonicity,
-    apply T₀.negate },
-  { apply hnq }
+  assume_negation with hq ,
+  simp [p_not_p_not_iff_self] at hq,
+  have occ := coincidence hq (liveness s Γ h ev),
+  clear h,
+  henceforth at occ ⊢,
+  eventually occ,
+  apply T₀.negate Γ occ
 end
 
 end soundness
@@ -147,15 +154,15 @@ def object (p : program lbl α) : target_mch (option lbl) :=
 { σ := α
 , s₀ := p.first
 , next := λ l s P, p.take_step l s
-, req := λ _, set.univ
-, req_nemp := λ _, @set.ne_empty_of_mem _ set.univ none trivial }
+, req := ↑λ _ : α, @set.univ (option lbl)
+, req_nemp := by { intros _, rw [set.ne_empty_iff_exists_mem], existsi none, trivial, } }
 
 lemma program.witness [sched lbl] (s : program lbl α)
 : ∃ (τ : stream (state (program lbl α))), ex s τ :=
 begin
   apply exists_imp_exists _ (sched.sched_str (object s)),
   intros τ h,
-  have H : ∀ e, •has_mem.mem e ∘ (object s).req ⋀ ⟦target_mch.action (object s) e⟧
+  have H : ∀ e, •(↑e ∊ (object s).req) ⋀ ⟦target_mch.action (object s) e⟧
               ⟹ ⟦s.action_of e⟧,
   { simp [init_eq_action,action_and_action],
     intro e,
@@ -166,26 +173,32 @@ begin
     intro, apply and.left },
   apply ex.mk,
   { rw h.init, refl },
-  { unfold saf_ex,
-    revert_p h.valid, monotonicity,
-    rw p_exists_entails_eq_p_forall_entails,
-    intro l,
-    transitivity, apply (H _),
-    revert l,
-    rw [← p_exists_entails_eq_p_forall_entails,exists_action], refl },
-  { intro e,
-    apply inf_often_entails_inf_often _ _ (h.fair e _),
-    { apply H },
-    { have H : has_mem.mem e ∘ (object s).req = True,
-      { refl },
-      rw [H,init_true,event_true,hence_true],
-      apply trivial } },
+  replace h := h.valid,
+  begin [temporal]
+    simp [saf_ex],
+    henceforth at h ⊢,
+    cases h with l h,
+    have := (H l Γ h),
+    revert this,
+    action {
+      simp [unitb.step,has_safety.step,is_step],
+      intro h, exact ⟨_,h⟩ },
+  end,
+  intro e, have H' := h.fair e,
+  begin [temporal]
+    note H'' : •(↑e ∊ (object s).req) = True,
+    { simp [temporal.init], congr, },
+    simp [H''] at H',
+    revert H', monotonicity,
+    intros H', apply H e Γ _,
+    simp [H''], apply H',
+  end
 end
 
 instance {α} [sched lbl] : system_sem (program lbl α) :=
   { (_ : system (program lbl α)) with
-    ex := λ p τ, ex p τ
-  , safety := @ex.safety _ _
+    ex := λ p, (↑(ex p) : cpred _)
+  , safety := safety
   , inhabited := program.witness
   , init_sem := @init_sem _ _
   , transient_sem := @transient.semantics _ _  }
